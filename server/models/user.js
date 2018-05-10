a343f082127218b0cf40ba11c053e336340406f3
seabassgonzalez <seabassgonzalez@gmail.com>
2018-05-10 12:36:54 -0700

    Implement hook function to run before save that uses bcrypt to encrypt password

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,23 @@
 // create const mongoose to hold require mongoose
 // create const Schema to pull Schema off of mongoose
+// create const bcrypt to import bcrypt password encryption
 
 // Define model
 // create const userSchema that's a new Schema
 	// email set to String type, but wrap in object and additional property to ensure user uniquenes, and another lowercase set to true to make sure all usernames unique
 	// password set to String type
+
+// Make an On Save Hook to encrypt password
+// call .pre() on userSchema passing it save and function taking next -- will run before saving a model
+	// const user set to this instance of user model
+	// brcypt.genSalt 10 and function taking error and salt -- to generate a salt and wait for it to complete before running callback
+		// handle error if error
+			// return next (error)
+		// bcrypt.hash() passing it user.password salt null and error handling callback -- to encrypt password
+			// if error
+				// return next(error)
+			// user.password = hash -- to overwrite plaintext password with resulting encrypted hash password
+			// next
 
 // Create the model class
 // create const ModelClass set to mongoose.model() passing it user and userSchema
@@ -13,10 +26,27 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
 	password: String
+});
+
+userSchema.pre('save', function(next){
+	const user = this;
+	bcrypt.genSalt(10, function(err, salt){
+		if(err){
+			return next(err);
+		}
+		bcrypt.hash(user.password, salt, null, function(err, hash){
+			if(err){
+				return next(err);
+			}
+			user.password = hash;
+			next();
+		});
+	});
 });
 
 const ModelClass = mongoose.model('user', userSchema);
